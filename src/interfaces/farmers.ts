cef5a12a0c25e4f6fee6a5b07b78cfb120aecdea
Kevin Ithagu <ithagusk@gmail.com>
2022-06-01 17:37:13 +0300

    feat: configured typing for data

---
+++ src/interfaces/farmers.ts
@@ -0,0 +1,61 @@
+export interface IFarmer {
+  id: number;
+  company_id: string;
+  version: number;
+  created_on: string;
+  created_by: string;
+  last_modified_on: string;
+  last_modified_by: string;
+  farmer_code: string;
+  sale_agent_code: string;
+  id_no: string;
+  county: string;
+  ward: string;
+  value_chaim: string;
+  farm_size: string;
+  photo: string;
+  signature: string;
+  additional_fields: string;
+  phone_number: string;
+  full_names: string;
+  farmer_id: string;
+  payment_method: "MPESA" | "CASH" | "BANK";
+  account_number: string;
+  account_name: string;
+  status:
+    | "PENDING_REGISTRATION"
+    | "REGISTERED"
+    | "ACTIVE"
+    | "PENDING"
+    | "DEACTIVATED"
+    | "DORMANT";
+  new: boolean;
+}
+
+// columns added
+export interface IFarmerDGResponse extends IFarmer {
+  activation_stage?:
+    | "PENDING_REGISTRATION"
+    | "SELF_REGISTRATION"
+    | "BRANCH_ACCOUNT"
+    | "CONVETIONAL_ACCOUNT";
+  deposit_limit?: number;
+  loan_limit?: number;
+  email_validation?: number;
+  crb_validation?: number;
+  phone_number_validation?: number;
+  phone_number_ownership_validation?: number;
+  iprs_validation?: number;
+  imsi_validation?: number;
+  nip?: number;
+  login_attempts?: number;
+}
+
+export interface IFarmerRegistration {
+  sale_agent_code: string;
+  id_no: string;
+  phone_number: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+}
---
+++ src/interfaces/loans.ts
@@ -0,0 +1,32 @@
+export interface ILoan {
+  farmer_id: 288;
+  loan_product_code: "LP-XWZ48QVG";
+  agrodealer_code: null;
+  reason: "string";
+  created_on: null;
+  code: "LON-0QP5R2GPGFJ3";
+  requested_amount: 1;
+  interest_rate: 0;
+  interest: 0;
+  fees: 0;
+  principal_amount: 1;
+  total_amount: 1;
+  installments: null;
+  installment_amount: null;
+  repayment_start_date: null;
+  status: string;
+  payment_status: "PENDING";
+  expected_completion_date: null;
+  paid: 0;
+  balance: 1;
+  product_code: null;
+  product_name: "Dairy Loan";
+  applicant_name: "Mary Otieno";
+  applicant_phone: "0708321651";
+  applicant_address: null;
+  applicant_email: null;
+  applicant_code: "32711464";
+  receiver_account_name: null;
+  receiver_account_number: null;
+  receiver_bank_name: null;
+}
