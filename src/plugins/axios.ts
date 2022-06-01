ca5845c3f229b7a8fba6e0be65b85e5c487916ad
Kevin Ithagu <ithagusk@gmail.com>
2022-06-01 17:37:49 +0300

    feat: axios configuration

---
+++ src/plugins/axios.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { notification } from "ant-design-vue";
+
+import router from "@/router";
+
+const $http = {
+  Authentication: axios.create({
+    baseURL: import.meta.env.VITE_VUE_APP_API_BASE_URL,
+    // headers: {
+    //   // "Access-Control-Allow-Origin": "*",
+    //   accept: "*/*",
+    //   "Content-Type": "application/json",
+    // },
+  }),
+
+  Api: axios.create({
+    baseURL: import.meta.env.VITE_VUE_APP_API_BASE_URL,
+    headers: {
+      // "Access-Control-Allow-Origin": "*",
+      accept: "*/*",
+      "Content-Type": "application/json",
+    },
+  }),
+};
+
+const accessToken = () => localStorage.getItem("access_token");
+
+const authenticationInterceptor = (config: AxiosRequestConfig) => {
+  // eslint-disable-next-line no-param-reassign
+  config.headers = {
+    Authorization: `Bearer ${accessToken()}`,
+  };
+  return config;
+};
+$http.Api.interceptors.request.use(authenticationInterceptor);
+$http.Api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      router.push("/unauthorized").then(() => {
+        localStorage.clear();
+      });
+      notification.error({
+        message: "Error",
+        description: error.response?.data?.error,
+      });
+    } else if (error.response.status === 403) {
+      router.push("/forbidden");
+      notification.error({
+        message: "Error",
+        description: error.response?.data?.error,
+      });
+    } else if (error.response.status === 500) {
+      notification.error({
+        message: "Error",
+        description: error.response?.data?.error,
+      });
+      router.push("/internal-server-error");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default $http;
